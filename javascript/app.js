2cb7c88dd4ad0ca5c6ff4ed7db639ce7f54aa4c2
Imran Syed <syed.imran2512@gmail.com>
2018-10-15 21:08:14 -0400

    started javascript

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+var simonSequence = []; //array to store sequence of Simon's moves
+var mySequence = []; //array to store the player sequence
+var counter = 0; // determines if it's Simon's or player's move
+var roundCounter = 0; // currnet game round
+var isDown = false; // to determine when the mouse is pressed down
+var playerTimer = false; //determines when timed period begins for a move
+var winFlashes = 0; // display winning signal
+var $colors = $('.section'); // each of the clored sections stored in an array
+var difficulty = "easy"; // default difficulty setting
